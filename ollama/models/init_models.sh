0a7e1d662870cfab0edb0004ace7801dbb855a53
argonism <44169535+argonism@users.noreply.github.com>
2025-09-17 22:05:46 +0900

    Add qwen

--- ollama/models/init_models.sh
+++ ollama/models/init_models.sh
@@ -4,6 +4,7 @@ set -e
 echo "Creating models via Ollama daemon..."
 
 ollama pull gpt-oss:20b
-# ollama create sarashina-3b -f /ollama/models/Modelfile.sarashina-3b
+ollama create sarashina-3b -f /ollama/models/Modelfile.sarashina-3b
+ollama pull qwen3:8b
 
 echo "All custom models created successfully."
